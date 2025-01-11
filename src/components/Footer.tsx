68195acab626d361989cca6d8a6d4fdcbd1a95c3
Alexander Fuks <kungfux@users.noreply.github.com>
2025-01-11 05:54:01 +0400

    fix: footer text for light theme

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="text-white py-4 flex justify-center text-s">
+    <footer className="text-black py-4 flex justify-center text-s dark:text-white">
       <p className="my-4">
         Binary Timer ❤️👨‍💻🚀 kungfux &copy; {new Date().getFullYear()}
       </p>
